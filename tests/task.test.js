7767aef2b326a5d87d4f66ba1f55f6c017008797
sychova <anastasiya.sychova@gmail.com>
2021-12-06 02:04:29 +0300

    fixing branch

--- tests/task.test.js
+++ tests/task.test.js
@@ -2,46 +2,153 @@ const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
 const {
-    user0Id,
-    user0,
-    user1Id,
-    user1,
+    userTask0Id,
+    userTask0,
+    userTask1Id,
+    userTask1,
     task0,
     task1,
     task2,
-    setupDatabase
-} = require('./fixtures/db')
+    setupTaskDatabase
+} = require('./fixtures/tasksDB')
 
-beforeEach(setupDatabase)
+beforeEach(setupTaskDatabase)
 
-test('Should create task for user', async() => {
-    const response = await request(app)
-        .post('/tasks')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send({
-            title: 'Buy a box'
+describe('Creating tasks', () => {
+    describe('Should do', () => {
+        it('Creates task for user', async() => {
+            const response = await request(app)
+                .post('/tasks')
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send({
+                    title: 'Buy a box'
+                })
+                .expect(201)
+            const task = await Task.findById(response.body._id)
+            expect(task).not.toBeNull()
+            expect(task.completed).toBe(false)
         })
-        .expect(201)
-    const task = await Task.findById(response.body._id)
-    expect(task).not.toBeNull()
-    expect(task.completed).toBe(false)
+    })
+    describe('Should not do', () => {
+        it('Should not create task with invalid title', async() => {
+            await request(app)
+                .post('/tasks')
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send({
+                    completed: true
+                })
+                .expect(400)
+        })
+    })
 })
 
-test('Should return all tasks of user 0', async() => {
-    const response = await request(app)
-        .get('/tasks')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send()
-        .expect(200)
-    expect(response.body.length).toEqual(2)
+describe('Getting tasks', () => {
+    describe('Should do', () => {
+        it('Returns all tasks of user 0', async() => {
+            const response = await request(app)
+                .get('/tasks')
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(200)
+            expect(response.body.length).toEqual(2)
+        })
+        
+        it('Fetches user task by id', async() => {
+            await request(app)
+                .get(`/tasks/${task0._id}`)
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(200)
+        })
+        it('Fetches only completed tasks', async() => {
+            const response = await request(app)
+                .get('/tasks?completed=true')
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(200)
+            expect(response.body).toHaveLength(1)
+        })
+        
+        it('Fetches only incomplete tasks', async() => {
+            const response = await request(app)
+                .get('/tasks?completed=false')
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(200)
+            expect(response.body).toHaveLength(1)
+        })
+    })
+    describe('Should not do', () => {
+        it('Should not fetch user task by id if unauthenticated', async() => {
+            await request(app)
+                .get(`/tasks/${task0._id}`)
+                .send()
+                .expect(401)
+        })
+        
+        it('Should not fetch other users task by id', async() => {
+            await request(app)
+                .get(`/tasks/${task2._id}`)
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(404)
+        })
+    })
 })
 
-test('Should not allow deleting tasks that the user doesn\'t own', async() => {
-    const response = await request(app)
-        .delete(`/tasks/${task0._id}`)
-        .set('Authorization', `Bearer ${user1.tokens[0].token}`)
-        .send()
-        .expect(404)
-    const task = await Task.findById(task0._id)
-    expect(task).not.toBeNull()
+describe('Updating tasks', () => {
+    describe('Should not do', () => {
+        it('Should not update task with invalid title', async() => {
+            await request(app)
+                .patch(`/tasks/${task1._id}`)
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send({
+                    title: ''
+                }).expect(400)
+            const task = await Task.findById(task1._id)
+            expect(task.title).toBeTruthy()
+        })
+        
+        it('Should not update other users task', async() => {
+            await request(app)
+                .patch(`/tasks/${task2._id}`)
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(404)
+        })
+    })
+})
+
+describe('Deleting tasks', () => {
+    describe('Should do', () => {
+        it('Deletes user task', async() => {
+            await request(app)
+                .delete(`/tasks/${task1._id}`)
+                .set('Authorization', `Bearer ${userTask0.tokens[0].token}`)
+                .send()
+                .expect(200)
+            const task = await Task.findById(task1._id)
+            expect(task).toBeNull()
+        })
+    })
+    describe('Should not do', () => {
+        it('Should not allow deleting tasks that the user doesn\'t own', async() => {
+            await request(app)
+                .delete(`/tasks/${task0._id}`)
+                .set('Authorization', `Bearer ${userTask1.tokens[0].token}`)
+                .send()
+                .expect(404)
+            const task = await Task.findById(task0._id)
+            expect(task).not.toBeNull()
+        })
+        
+        it('Should not delete task if unauthenticated', async() => {
+            await request(app)
+                .delete(`/tasks/${task1._id}`)
+                .send()
+                .expect(401)
+            const task = await Task.findById(task1._id)
+            expect(task).not.toBeNull()
+        })
+    })
 })
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,161 +1,145 @@
 const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
-const { user0Id, user0, setupDatabase } = require('./fixtures/db')
+const { 
+    user0Id,
+    user0,
+    user1Id,
+    user1,
+    setupUserDatabase
+} = require('./fixtures/usersDB')
 
-beforeEach(setupDatabase)
+beforeAll(setupUserDatabase)
 
-test('Should signup a new user', async() => {
-    const response = await request(app)
-        .post('/users')
-        .send({
-            name: 'Anastasiya',
-            email: 'a.sychova@sumatosoft.com',
-            password: 'Qwerty1234'
-        }).expect(201)
+describe('User signup', () => {
+    describe('Should do', () => {
+        it('Signs up a new user', async() => {
+            const response = await request(app)
+            .post('/users')
+            .send({
+                name: 'Anastasiya',
+                email: 'a.sychova@sumatosoft.com',
+                password: 'Qwerty1234'
+            })
+            .expect(201)
 
-    // Assert db changed correctly
-    const user = await User.findById(response.body.user._id)
-    expect(user).not.toBeNull()
+            const user = await User.findById(response.body.user._id)
+            expect(user).not.toBeNull()
 
-    // Assertions about the response
-    // expect(response.body.user.name).toBe('Anastasiya')
-    expect(response.body).toMatchObject({
-        user: {
-            name: 'Anastasiya',
-            email: 'a.sychova@sumatosoft.com'
-        },
-        token: user.tokens[0].token
+            expect(response.body).toMatchObject({
+                user: {
+                    name: 'Anastasiya',
+                    email: 'a.sychova@sumatosoft.com'
+                },
+                token: user.tokens[0].token
+            })
+            expect(user.password).not.toBe('Qwerty1234')
+        })
     })
-    expect(user.password).not.toBe('Qwerty1234')
 })
 
-test('Should not signup user with invalid email', async() => {
-    const response = await request(app)
-        .post('/users')
-        .send({
-            name: 'Anastasiya',
-            email: user0.email,
-            password: user0.password
-        }).expect(400)
-})
-
-test('Should not signup user with invalid password', async() => {
-    const response = await request(app)
-        .post('/users')
-        .send({
-            name: 'Anastasiya',
-            email: 'a.sychova@sumatosoft.com',
-            password: 'password'
-        }).expect(400)
-})
-
-test('Should login existing user', async() => {
-    const response = await request(app)
-        .post('/users/login')
-        .send({
-            email: user0.email,
-            password: user0.password
-        }).expect(200)
-    const user = await User.findById(user0Id)
-    expect(response.body.token).toBe(user.tokens[1].token)
-})
-
-test('Should not login nonexistent user', async() => {
-    await request(app)
-        .post('/users/login')
-        .send({
-            email: 'qqqqq@qqqqqq.qqqqq',
-            password: 'qqqqqqqq'
-        }).expect(400)
-})
-
-test('Should get profile for user', async() => {
-    await request(app)
-        .get('/users/me')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send()
-        .expect(200)
-})
-
-test('Should not get profile for unauthenticated user', async() => {
-    await request(app)
-        .get('/users/me')
-        .send()
-        .expect(401)
-})
-
-test('Should delete account for user', async() => {
-    const response = await request(app)
-        .delete('/users/me')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send()
-        .expect(200)
-    const user = await User.findById(response.body._id)
-    expect(user).toBeNull()
-})
-
-test('Should not delete account for unauthenticated user', async() => {
-    await request(app)
-        .delete('/users/me')
-        .send()
-        .expect(401)
-})
-
-test('Should upload avatar image', async() => {
-    await request(app)
-        .post('/users/me/avatar')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
-        .expect(200)
-    const user = await User.findById(user0Id)
-    expect(user.avatar).toEqual(expect.any(Buffer))
-})
-
-test('Should update valid user fields', async() => {
-    await request(app)
-        .patch('/users/me')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send({
-            name: 'Anastasiya'
+describe('User login', () => {
+    describe('Should do', () => {
+        it('Logs in existing user', async() => {
+            const response = await request(app)
+            .post('/users/login')
+            .send({
+                email: user0.email,
+                password: user0.password
+            })
+            .expect(200)
+            const user = await User.findById(user0Id)
+            expect(response.body.token).toBe(user.tokens[1].token)
         })
-        .expect(200)
-    const user = await User.findById(user0Id)
-    expect(user.name).toEqual('Anastasiya')
-})
-
-test('Should not update invalid user fields', async() => {
-    await request(app)
-        .patch('/users/me')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send({
-            location: 'Minsk'
+    })
+    describe('Should not do', () => {
+        it('Should not login nonexistent user', async() => {
+            await request(app)
+            .post('/users/login')
+            .send({
+                email: 'qqqqq@qqqqqq.qqqqq',
+                password: 'qqqqqqqq'
+            })
+            .expect(400)
         })
-        .expect(400)
+    })
 })
 
-test('Should not update user with invalid email', async() => {
-    await request(app)
-        .patch('/users/me')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send({
-            email: 'anastasiya.sychova@example.com'
-        }).expect(400)
+describe('Getting Users', () => {
+    describe('Should do', () => {
+        it('Gets profile for user', async() => {
+            await request(app)
+                .get('/users/me')
+                .set('Authorization', `Bearer ${user0.tokens[0].token}`)
+                .send()
+                .expect(200)
+        })
+    })
+    describe('Should not do', () => {
+        it('Should not get profile for unauthenticated user', async() => {
+            await request(app)
+                .get('/users/me')
+                .send()
+                .expect(401)
+        })
+    })
 })
 
-test('Should not update user with invalid password', async() => {
-    await request(app)
-        .patch('/users/me')
-        .set('Authorization', `Bearer ${user0.tokens[0].token}`)
-        .send({
-            password: 'password'
-        }).expect(400)
+describe('Updating Users', () => {
+    describe('Should do', () => {
+        it('Uploads avatar image', async() => {
+            await request(app)
+                .post('/users/me/avatar')
+                .set('Authorization', `Bearer ${user0.tokens[0].token}`)
+                .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+                .expect(200)
+            const user = await User.findById(user0Id)
+            expect(user.avatar).toEqual(expect.any(Buffer))
+        })
+        
+        it('Updates valid user fields', async() => {
+            await request(app)
+                .patch('/users/me')
+                .set('Authorization', `Bearer ${user0.tokens[0].token}`)
+                .send({
+                    name: 'Anastasiya'
+                })
+                .expect(200)
+            const user = await User.findById(user0Id)
+            expect(user.name).toEqual('Anastasiya')
+        })
+    })
+    describe('Should not do', () => {
+        test('Should not update invalid user fields', async() => {
+            await request(app)
+                .patch('/users/me')
+                .set('Authorization', `Bearer ${user0.tokens[0].token}`)
+                .send({
+                    location: 'Minsk'
+                })
+                .expect(400)
+        })
+    })
 })
 
-test('Should not update user if unauthenticated', async() => {
-    await request(app)
-        .patch('/users/me')
-        .send({
-            name: 'Minsk'
-        }).expect(401)
+describe('Deleting Users', () => {
+    describe('Should do', () => {
+        test('Deletes account for user', async() => {
+            const response = await request(app)
+                .delete('/users/me')
+                .set('Authorization', `Bearer ${user1.tokens[0].token}`)
+                .send()
+                .expect(200)
+            const user = await User.findById(response.body._id)
+            expect(user).toBeNull()
+        })
+    })
+    describe('Should not do', () => {
+        test('Should not delete account for unauthenticated user', async() => {
+            await request(app)
+                .delete('/users/me')
+                .send()
+                .expect(401)
+        })
+    })
 })
